f8aaeed00547b1b4ac9bcc0895c72d750f7ab680
Gilbert <shikweg@gmail.com>
2024-02-05 13:38:51 +0300

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,12 +4,18 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // Cleanup function to stop the interval when the component is unmounted
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
 }
 
 export default Clock;
+
